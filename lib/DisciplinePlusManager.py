2ae77f2b6959c0ba19f51baca10708485c92da1e
AshwaniDev101 <ashwani.yadav.dev@gmail.com>
2025-06-04 18:40:11 +0530

    some refactoring

--- lib/DisciplinePlusManager.py
+++ lib/DisciplinePlusManager.py
@@ -6,7 +6,7 @@ from lib.managers.json.shared_preferences import SharedPreferences
 class DisciplinePlusManager:
     def __init__(self):
 
-        self.day_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        self.weekday_name_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
         firebase_service_account_key_path = "managers/firebase/key/firebase-discipline-plus-serviceAccountKey.json"
 
@@ -15,13 +15,13 @@ class DisciplinePlusManager:
 
     def upload_excel_to_firebase(self):
 
-        for day in self.day_list:
+        for weekday_name in self.weekday_name_list:
             # getting data from excel file
-            initiative_list = self.excel_sheet_manager.get_initiative_list_from_excel(day)
+            initiative_list = self.excel_sheet_manager.get_initiative_list_from_excel(weekday_name)
 
             # uploading that data to firebase
-            self.firebase_manger.upload_initiative_list_to_firebase(day=day, data_list=initiative_list)
+            self.firebase_manger.upload_initiative_list_to_firebase(day=weekday_name, data_list=initiative_list)
 
     def clean_firebase_data(self):
-        for day in self.day_list:
+        for day in self.weekday_name_list:
             self.firebase_manger.clean_initiatives(day)
